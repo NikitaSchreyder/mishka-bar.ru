fb52bb06e800f00060ce3bb03ef39c73047850f7
Nikita Schreyder <nikitaschreyder@gmail.com>
2023-04-14 00:48:04 +0500

    add interior images

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,10 +1,19 @@
+import { join } from 'path'
 import { Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
 
 import { BarModule } from '../bar/bar.module'
 import { MenuModule } from '../menu/menu.module'
 
 @Module({
-  imports: [BarModule, MenuModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '/public'),
+      serveRoot: '/public/'
+    }),
+    BarModule, 
+    MenuModule
+  ],
   controllers: [],
   providers: [],
 })
