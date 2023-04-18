0544ba5a9cb61190fbfc36951f71aa5406bbec47
Nikita Schreyder <nikitaschreyder@gmail.com>
2023-04-18 23:11:16 +0300

    da

--- frontend/src/core/modals/components/PickupModalContent.tsx
+++ frontend/src/core/modals/components/PickupModalContent.tsx
@@ -2,7 +2,9 @@ const PickupModalContent: React.FC = () => {
     return (
         <div className="layout_container">
             <div className="pickup-modal_content">
-                <p></p>
+                <p>
+                    Для оформления заказа позвоните по телефону
+                </p>
             </div>
         </div>
     )
