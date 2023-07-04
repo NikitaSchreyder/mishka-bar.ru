96c424283f213c6af4158174a183b918e39cf38e
Nikita Schreyder <nikitaschreyder@gmail.com>
2023-07-04 21:26:32 +0500

    admin panel

--- backend/src/menu/menu.module.ts
+++ backend/src/menu/menu.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common'
+import { SequelizeModule } from '@nestjs/sequelize'
+
 import { MenuService } from './menu.service'
 import { MenuController } from './menu.controller'
+import { FilesModule } from '../files/files.module'
+import { MenuCategoriesModel } from './models/menu-categories.model'
 
 @Module({
+  imports: [
+    SequelizeModule.forFeature([
+      MenuCategoriesModel
+    ]),
+    FilesModule
+  ],
   providers: [MenuService],
   controllers: [MenuController]
 })
--- backend/src/menu/menu.service.ts
+++ backend/src/menu/menu.service.ts
@@ -1,23 +1,121 @@
-import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/sequelize';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
-import { menuItems } from './data/items'
-import { menuCategories } from './data/catgories'
-import { TMenuCategory } from 'src/app/types/types'
+import { FilesService } from '../files/files.service';
+import { MenuCategoriesModel } from './models/menu-categories.model';
+import { CreateMenuCategoryDto, RemoveMenuCategoryDto, UpdateMenuCategoryDto } from './dto/menu-categories.dto';
+
+import { transliterate } from '../core/text-utils/translit';
 
 @Injectable()
 export class MenuService {
-    public getCategories(): TMenuCategory[] {
-        return menuCategories
+    constructor(
+        @InjectModel(MenuCategoriesModel) 
+        readonly menuCategoriesRepository: typeof MenuCategoriesModel,
+        readonly filesSefvice: FilesService
+    ) {}
+
+    public categories = {
+        get: async () => {
+            const categories = await this.menuCategoriesRepository.findAll()
+            if(!categories) throw new HttpException('Произошла ошибка сервера', HttpStatus.INTERNAL_SERVER_ERROR)
+            return categories
+        },
+        create: async (dto: CreateMenuCategoryDto, file: Express.Multer.File) => {
+            await this.checkCategoryDuplicate(dto.name)
+            
+            const searchLink = transliterate(dto.name).toLowerCase()
+            const thumbUrl = await this.filesSefvice.savePhoto(file)
+            const newCategoryData: CreateMenuCategoryDto = { ...dto, searchLink, thumbUrl }
+            const newCategory = this.menuCategoriesRepository.create(newCategoryData)
+
+            if(!newCategory) throw new HttpException('Произошла ошибка сервера', HttpStatus.INTERNAL_SERVER_ERROR)
+            if(newCategory) throw new HttpException(`Категория "${(await newCategory).name}" успешно создана`, HttpStatus.OK)
+        },
+        update: async (dto: UpdateMenuCategoryDto, file: Express.Multer.File) => {
+            const menuCategory = await this.throwIfCategoryNotFound(dto.id)
+            
+            if(!file) {
+                const updateStatus = await menuCategory.update(dto)
+                if(!updateStatus) throw new HttpException('Произошла ошибка сервера', HttpStatus.INTERNAL_SERVER_ERROR)
+                if(updateStatus) throw new HttpException(`Категория "${menuCategory.name}" успешно обновлена`, HttpStatus.OK)
+            }
+
+            if(file) {
+                const thumbUrl = await this.filesSefvice.savePhoto(file)
+                const newCategoryData: UpdateMenuCategoryDto = { ...dto, thumbUrl }
+                const updateStatus = await menuCategory.update(newCategoryData)
+                if(!updateStatus) throw new HttpException('Произошла ошибка сервера', HttpStatus.INTERNAL_SERVER_ERROR)
+                if(updateStatus) throw new HttpException(`Категория "${menuCategory.name}" успешно обновлена`, HttpStatus.OK)
+            } 
+        },
+        remove: async (dto: RemoveMenuCategoryDto) => {
+            const menuCategory = await this.throwIfCategoryNotFound(dto.id)
+            await menuCategory.destroy()
+            throw new HttpException(`Категория "${menuCategory.name}" успешно удалена`, HttpStatus.OK)
+        }
     }
 
-    public getCategoryItems(categorySearchLink: string) {
-        const categoryItems = menuItems.filter(item => item.categorySearchLink === categorySearchLink)
-        const categoryName = menuCategories.filter(item => item.searchLink === categorySearchLink)[0].name
-        const data = {
-            categoryItems,
-            categoryName
-        }
-        
-        return data
+    private async checkCategoryDuplicate(name: string) {
+        const isDuplicate = await this.menuCategoriesRepository.findOne({ where: {name}})
+        if(isDuplicate) throw new HttpException('Категория уже существует', HttpStatus.BAD_REQUEST)
     }
+
+    private async throwIfCategoryNotFound(id: number) {
+        const menuCategory = await this.menuCategoriesRepository.findOne({
+            where: {id}
+        })
+        if(!menuCategory) throw new HttpException('Категория не найдена', HttpStatus.NOT_FOUND)
+        return menuCategory
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // public getCategories(): TMenuCategory[] {
+    //     return menuCategories
+    // }
+
+    // public getCategoryItems(categorySearchLink: string) {
+    //     const categoryItems = menuItems.filter(item => item.categorySearchLink === categorySearchLink)
+    //     const categoryName = menuCategories.filter(item => item.searchLink === categorySearchLink)[0].name
+    //     const data = {
+    //         categoryItems,
+    //         categoryName
+    //     }
+        
+    //     return data
+    // }
 }
