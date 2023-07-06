1a4f3c180be53e28177801536f82793736dee2e5
Nikita Schreyder <nikitaschreyder@gmail.com>
2023-07-06 23:20:47 +0500

    admin page done

--- frontend/src/admin/modals/AdminModals.tsx
+++ frontend/src/admin/modals/AdminModals.tsx
@@ -1,9 +1,29 @@
 import { Modal, Spin } from "antd"
 import dynamic from "next/dynamic"
-import CreateDishModalContent from './components/CreateDishModalContent'
 
 const UpdateDishModalContent = dynamic(() => 
     import('./components/UpdateDishModalContent'), 
+    {
+        loading: () => <Spin />
+    }
+)
+
+const CreateDishModalContent = dynamic(() => 
+    import('./components/CreateDishModalContent'), 
+    {
+        loading: () => <Spin />
+    }
+)
+
+const UpdateCategoryModalContent = dynamic(() => 
+    import('./components/UpdateCategoryModalContent'), 
+    {
+        loading: () => <Spin />
+    }
+)
+
+const CreateCategoryModalContent = dynamic(() => 
+    import('./components/CreateCategoryModalContent'), 
     {
         loading: () => <Spin />
     }
@@ -39,4 +59,36 @@ export const CreateDishModal: React.FC<{open: boolean, closeModal: () => void}> 
             <CreateDishModalContent />
         </Modal>
     )
+}
+
+export const UpdateCategoryModal: React.FC<{open: boolean, closeModal: () => void, updatedItem: any}> = ({open, closeModal, updatedItem}) => {
+    return (
+        <Modal
+            wrapClassName="modal-bg"
+            open={open}
+            closable={false}
+            className="pickup-modal"
+            footer={null}
+            destroyOnClose={true}
+            onCancel={closeModal}
+        >
+            <UpdateCategoryModalContent updatedItem={updatedItem} />
+        </Modal>
+    )
   }
+
+export const CreateCategoryModal: React.FC<{open: boolean, closeModal: () => void}> = ({open, closeModal}) => {
+    return (
+        <Modal
+            wrapClassName="modal-bg"
+            open={open}
+            closable={false}
+            className="pickup-modal"
+            footer={null}
+            destroyOnClose={true}
+            onCancel={closeModal}
+        >
+            <CreateCategoryModalContent />
+        </Modal>
+    )
+}
