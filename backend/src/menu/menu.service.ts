cb844b845ba1dfc743ddb2c16d4a3e09982d4fbc
Nikita Schreyder <nikitaschreyder@gmail.com>
2023-07-06 15:59:37 +0500

    menu admin backend done

--- backend/src/menu/menu.service.ts
+++ backend/src/menu/menu.service.ts
@@ -3,7 +3,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 import { FilesService } from '../files/files.service';
 import { MenuCategoriesModel } from './models/menu-categories.model';
-import { CreateMenuCategoryDto, CreateMenuDishDto, RemoveMenuCategoryDto, UpdateMenuCategoryDto } from './dto/menu-categories.dto';
+import { CreateMenuCategoryDto, CreateMenuDishDto, RemoveMenuCategoryDto, RemoveMenuDishDto, UpdateMenuCategoryDto, UpdateMenuDishDto } from './dto/menu-categories.dto';
 
 import { transliterate } from '../core/text-utils/translit';
 import { MenuDishesModel } from './models/menu-dishes.model';
@@ -25,6 +25,7 @@ export class MenuService {
         },
         create: async (dto: CreateMenuCategoryDto, file: Express.Multer.File) => {
             await this.checkCategoryDuplicate(dto.name)
+            if(!file) throw new HttpException('Фото категории обязательно', HttpStatus.BAD_REQUEST)
             
             const searchLink = transliterate(dto.name).toLowerCase()
             const thumbUrl = await this.filesSefvice.savePhoto(file)
@@ -38,7 +39,12 @@ export class MenuService {
             const menuCategory = await this.throwIfCategoryNotFound(dto.id)
             
             if(!file) {
-                const updateStatus = await menuCategory.update(dto)
+                const searchLink = transliterate(dto.name).toLowerCase()
+                const updateData: UpdateMenuCategoryDto = {
+                    ...dto,
+                    searchLink
+                } 
+                const updateStatus = await menuCategory.update(updateData)
 
                 if(!updateStatus) throw new HttpException('Произошла ошибка сервера', HttpStatus.INTERNAL_SERVER_ERROR)
                 if(updateStatus) throw new HttpException(`Категория "${menuCategory.name}" успешно обновлена`, HttpStatus.OK)
@@ -46,8 +52,14 @@ export class MenuService {
 
             if(file) {
                 const thumbUrl = await this.filesSefvice.savePhoto(file)
-                const newCategoryData: UpdateMenuCategoryDto = { ...dto, thumbUrl }
-                const updateStatus = await menuCategory.update(newCategoryData)
+                const searchLink = transliterate(dto.name).toLowerCase()
+                const updateData: UpdateMenuCategoryDto = {
+                    ...dto,
+                    searchLink,
+                    thumbUrl
+                } 
+
+                const updateStatus = await menuCategory.update(updateData)
 
                 if(!updateStatus) throw new HttpException('Произошла ошибка сервера', HttpStatus.INTERNAL_SERVER_ERROR)
                 if(updateStatus) throw new HttpException(`Категория "${menuCategory.name}" успешно обновлена`, HttpStatus.OK)
@@ -82,6 +94,10 @@ export class MenuService {
         create: async (dto: CreateMenuDishDto, file: Express.Multer.File) => {
             await this.checkDishDuplicate(dto.name)
 
+            if(!file) throw new HttpException('Фото блюда обязательно', HttpStatus.BAD_REQUEST)
+            if(!dto.price) throw new HttpException('Цена блюда обязательна', HttpStatus.BAD_REQUEST)
+            if(!dto.composition) throw new HttpException('Состав блюда обязателен', HttpStatus.BAD_REQUEST)
+
             const category = await this.throwIfCategoryBySearchLinkNotFound(dto.categorySearchLink)
             const searchLink = transliterate(dto.name).toLowerCase()
             const thumbUrl = await this.filesSefvice.savePhoto(file)
@@ -93,6 +109,46 @@ export class MenuService {
 
             return newDish
         },
+        update: async (dto: UpdateMenuDishDto, file: Express.Multer.File) => {
+            const dish = await this.throwIfDishNotFound(dto.id)
+            
+            if(dto.categorySearchLink !== dish.categorySearchLink) 
+                await this.throwIfCategoryBySearchLinkNotFound(dto.categorySearchLink)
+
+            if(!file) {
+                const searchLink = transliterate(dto.name).toLowerCase()
+                const updateData: UpdateMenuDishDto = {
+                    ...dto,
+                    searchLink
+                } 
+
+                const updateStatus = await dish.update(updateData)
+
+                if(!updateStatus) throw new HttpException('Произошла ошибка сервера', HttpStatus.INTERNAL_SERVER_ERROR)
+                if(updateStatus) throw new HttpException(`Категория "${dish.name}" успешно обновлена`, HttpStatus.OK)
+            }
+
+            if(file) {
+                const searchLink = transliterate(dto.name).toLowerCase() 
+                const thumbUrl = await this.filesSefvice.savePhoto(file)
+                const updateData: UpdateMenuDishDto = {
+                    ...dto,
+                    searchLink,
+                    thumbUrl
+                } 
+
+                const updateStatus = await dish.update(updateData)
+
+                if(!updateStatus) throw new HttpException('Произошла ошибка сервера', HttpStatus.INTERNAL_SERVER_ERROR)
+                if(updateStatus) throw new HttpException(`Категория "${dish.name}" успешно обновлена`, HttpStatus.OK)
+            } 
+        },
+        remove: async (dto: RemoveMenuDishDto) => {
+            const dish = await this.throwIfDishNotFound(dto.id)
+            await dish.destroy()
+            
+            throw new HttpException(`Блюдо "${dish.name}" успешно удалено`, HttpStatus.OK)
+        }
     }
 
     private async checkDishDuplicate(name: string) {
@@ -103,6 +159,14 @@ export class MenuService {
     private async checkCategoryDuplicate(name: string) {
         const isDuplicate = await this.menuCategoriesRepository.findOne({ where: {name}})
         if(isDuplicate) throw new HttpException('Категория уже существует', HttpStatus.BAD_REQUEST)
+    }
+
+    private async throwIfDishNotFound(id: number) {
+        const dish = await this.menuDishesRepository.findOne({
+            where: {id}
+        })
+        if(!dish) throw new HttpException('Блюдо не найдено', HttpStatus.NOT_FOUND)
+        return dish
     }
 
     private async throwIfCategoryNotFound(id: number) {
