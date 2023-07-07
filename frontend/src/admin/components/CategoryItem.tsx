8dfd96de2121b731f1918c87449afe0efcf65fd0
Nikita Schreyder <nikitaschreyder@gmail.com>
2023-07-08 02:57:58 +0500

    little changes

--- frontend/src/admin/components/CategoryItem.tsx
+++ frontend/src/admin/components/CategoryItem.tsx
@@ -34,7 +34,7 @@ const CategoryItem: React.FC<ICategoryItemProps> = ({item, showUpdateModal, upda
   )
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column', maxWidth: 200, gap: 10}}>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: 200, gap: 10}}>
       <img src={`http://mishkabar.localhost${thumbUrl}`} width={200} alt="" />
       <p style={{color: 'white'}}>{name}</p>
       <Popover
--- frontend/src/admin/components/DishesItem.tsx
+++ frontend/src/admin/components/DishesItem.tsx
@@ -36,7 +36,7 @@ const DishesItem: React.FC<IDishesItemProps> = ({item, showUpdateModal, updateDi
   )
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column', maxWidth: 200, gap: 10}}>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: 200, gap: 10}}>
       <img src={`http://mishkabar.localhost${thumbUrl}`} width={200} alt="" />
       <p style={{color: 'white'}}>{name}</p>
       <Popover
