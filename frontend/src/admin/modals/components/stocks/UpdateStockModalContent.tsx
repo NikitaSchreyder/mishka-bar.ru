5b38d28d5667f8fdbabcea159a3bc586f76f6f1a
Nikita Schreyder <nikitaschreyder@gmail.com>
2023-07-08 02:57:58 +0500

    little changes

--- frontend/src/admin/modals/components/stocks/UpdateStockModalContent.tsx
+++ frontend/src/admin/modals/components/stocks/UpdateStockModalContent.tsx
@@ -1,10 +1,10 @@
 import { Button, Input, message } from 'antd'
 import { FormEvent } from 'react'
-import { useRouter } from 'next/router'
 import { axiosApi } from '../../../../core/api/AxiosApi'
 
 const UpdateStockModalContent: React.FC<{updatedItem: any, closeModal: () => void, updateStocks: () => void}> = ({updatedItem, closeModal, updateStocks}) => {
-  const router = useRouter()
+  const { TextArea } = Input;
+
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const formData = new FormData(e.currentTarget as HTMLFormElement)
@@ -28,7 +28,7 @@ const UpdateStockModalContent: React.FC<{updatedItem: any, closeModal: () => voi
         <Input style={{marginBottom: 20}} name='name' id="name" defaultValue={updatedItem.name} />
 
         <label style={{color: 'white', marginBottom: 10}} htmlFor='description'>Описание</label>
-        <Input style={{marginBottom: 20}} name='description' id="description" defaultValue={updatedItem.description} />
+        <TextArea rows={4} style={{marginBottom: 20}} name='description' id="description" defaultValue={updatedItem.description} />
 
         <Button htmlType='submit'>Сохранить</Button>
       </form>
