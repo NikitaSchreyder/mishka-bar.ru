7058d7d6d14e61f1239dce31cdabc31e5b7d023d
Nikita Schreyder <nikitaschreyder@gmail.com>
2023-07-10 14:28:32 +0500

    little changes

--- frontend/src/admin/panels/about/AdminAboutPanel.tsx
+++ frontend/src/admin/panels/about/AdminAboutPanel.tsx
@@ -1,5 +1,5 @@
 import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps'
-import { Image } from 'antd'
+import { Button, Image } from 'antd'
 import { createRef, useEffect, useState } from 'react'
 import { IAboutIndexPageProps } from '../../../about/types/types'
 import { axiosApi } from '../../../core/api/AxiosApi'
@@ -12,8 +12,9 @@ const AdminAboutPanel: React.FC = () => {
     id: '',
     thumbUrl: ''
   })
-  const [aboutImage, setAboutImage] = useState()
+  const [saveBtnVisible, setSaveBtnVisible] = useState<boolean>(false)
   const fileInputRef = createRef<HTMLInputElement>()
+  const descriptionInputRef = createRef<HTMLParagraphElement>()
 
   const mapState: ymaps.IMapState = { center: [61.258612, 73.403110], zoom: 17 }
 
@@ -23,6 +24,14 @@ const AdminAboutPanel: React.FC = () => {
     axiosApi().get('/about')
       .then(data => setAboutData(data.data))
   }, [])
+
+  const onDescriptionChange = () => {
+    if(descriptionInputRef.current?.textContent !== description) {
+      setSaveBtnVisible(true)
+    } else {
+      setSaveBtnVisible(false )
+    }
+  }
 
   const updatePhoto = (file: any) => {
     const formdata = new FormData()
@@ -34,24 +43,25 @@ const AdminAboutPanel: React.FC = () => {
       })
   }
 
-  const updateDescription = (text: string) => {
+  const updateDescription = () => {
     const formdata = new FormData()
-    formdata.append('description', text)
+    formdata.append('description', descriptionInputRef.current?.textContent as string)
     axiosApi(token).put('/about', formdata)
       .then(() => {
-        // axiosApi().get('/about')
-        //   .then(data => setAboutData(data.data))
+        axiosApi().get('/about')
+          .then(data => {setAboutData(data.data), setSaveBtnVisible(false)})
       })
   }
 
-
+  // onBlur={e => updateDescription(e.currentTarget.textContent as string)}
   return (
     <div style={{paddingTop: 50}}>
       <div className="layout_container">
         <div className="about-content">
           <Image style={{cursor: 'pointer'}} onClick={() => fileInputRef.current?.click()} alt='Фото зала' className='about-img' preview={false} src={thumbUrl} />
           <input onChange={e => updatePhoto(e.currentTarget.files?.item(0))} ref={fileInputRef} type="file" style={{display: 'none'}} />
-          <p contentEditable onBlur={e => updateDescription(e.currentTarget.textContent as string)} className="about-description">{description}</p>
+          <p ref={descriptionInputRef} onInput={onDescriptionChange} contentEditable className="about-description">{description}</p>
+          <Button style={{display: saveBtnVisible ? 'block' : 'none'}} onClick={updateDescription}>Сохранить описание</Button>
         </div>
       </div>
       <div className='about_map-container'>
@@ -59,7 +69,7 @@ const AdminAboutPanel: React.FC = () => {
           <Map className='about_map' state={mapState}>
             <Placemark geometry={mapState.center} />
           </Map>
-        </YMaps>
+        </YMaps>  
       </div>
     </div>
   )
