e6a2d21766653b6eab8d4fc2f2ccd9198a24ae6d
nikitaschreyder <nikitaschreyder@gmail.com>
2023-10-26 14:24:54 +0500

    fix pathes and nginx conf

--- admin/src/admin/components/DishesItem.tsx
+++ admin/src/admin/components/DishesItem.tsx
@@ -55,7 +55,7 @@ const DishesItem: React.FC<IDishesItemProps> = ({item, showUpdateModal, updateDi
 
   return (
     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: 200, gap: 10}}>
-      <img src={`http://mishkabar.localhost${thumbUrl}`} width={200} alt="" />
+      <img src={`${thumbUrl}`} width={200} alt="" />
       <p style={{color: 'white'}}>{name}</p>
       <Popover
         trigger={'click'}
