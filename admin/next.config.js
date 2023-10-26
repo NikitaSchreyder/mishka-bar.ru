e148f4d91ffb81a20addf1066abc200da18a959d
nikitaschreyder <nikitaschreyder@gmail.com>
2023-10-26 18:10:56 +0500

    зшявф

--- admin/next.config.js
+++ admin/next.config.js
@@ -3,13 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     apiUrl: 'http://api.mishkabar.localhost/',
-    publicUrl: 'http://public.mishkabar.localhost/',
-    links: {
-      vk: 'https://vk.com/club211107184',
-      tg: 'https://t.me/mishka_bar_surgut',
-      map: 'https://yandex.ru/maps/-/CCU0MRcY8C',
-      yafood: 'https://eda.yandex.ru/surgut/r/miska_1635151764'
-    }
+    publicUrl: 'http://public.mishkabar.localhost/'
   }
 }
 
