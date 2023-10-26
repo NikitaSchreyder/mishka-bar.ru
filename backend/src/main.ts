945f1d2e6f6e96f42e285c80592805f83c145e15
nikitaschreyder <nikitaschreyder@gmail.com>
2023-10-26 13:44:02 +0500

    fix pathes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  app.enableCors()
   app.setGlobalPrefix('api')
   app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(3007)
